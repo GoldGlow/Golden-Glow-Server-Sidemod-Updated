08de21e253f635fad0aa429a8cadf6a2515ebcdb
Ryan Vickers <infernape971@rocketmail.com>
2019-07-21 18:51:59 +0100

    Added null checks to Capability Storage

--- src/main/java/com/goldenglow/common/data/OOPlayerStorage.java
+++ src/main/java/com/goldenglow/common/data/OOPlayerStorage.java
@@ -13,17 +13,23 @@ public class OOPlayerStorage implements Capability.IStorage<IPlayerData> {
     @Override
     public NBTBase writeNBT(Capability<IPlayerData> capability, IPlayerData instance, EnumFacing side) {
         final NBTTagCompound tag = new NBTTagCompound();
-        tag.setString("theme_wild", instance.getWildTheme());
-        tag.setString("theme_trainer", instance.getTrainerTheme());
-        tag.setString("theme_pvp", instance.getPVPTheme());
+        if(instance.getWildTheme()!=null)
+            tag.setString("theme_wild", instance.getWildTheme());
+        if(instance.getTrainerTheme()!=null)
+            tag.setString("theme_trainer", instance.getTrainerTheme());
+        if(instance.getPVPTheme()!=null)
+            tag.setString("theme_pvp", instance.getPVPTheme());
         return tag;
     }
 
     @Override
     public void readNBT(Capability<IPlayerData> capability, IPlayerData instance, EnumFacing side, NBTBase nbt) {
         final NBTTagCompound tag = (NBTTagCompound)nbt;
-        instance.setWildTheme(tag.getString("theme_wild"));
-        instance.setTrainerTheme(tag.getString("theme_trainer"));
-        instance.setPVPTheme(tag.getString("theme_pvp"));
+        if(tag.hasKey("theme_wild"))
+            instance.setWildTheme(tag.getString("theme_wild"));
+        if(tag.hasKey("theme_trainer"))
+            instance.setTrainerTheme(tag.getString("theme_trainer"));
+        if(tag.hasKey("theme_pvp"))
+            instance.setPVPTheme(tag.getString("theme_pvp"));
     }
 }
